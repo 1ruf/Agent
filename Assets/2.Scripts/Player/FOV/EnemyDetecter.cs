34651578b5f095137c624339117989b26bc506cf
1_ruf <gyeol0411@naver.com>
2024-08-21 09:53:20 +0900

    AddViewAngle
    add FOV

--- Assets/2.Scripts/Player/FOV/EnemyDetecter.cs
+++ Assets/2.Scripts/Player/FOV/EnemyDetecter.cs
@@ -7,19 +7,25 @@ public class EnemyDetecter : MonoBehaviour
     [SerializeField] private GameObject enemy;
     [SerializeField] private EnemyMovement enemyScript;
     [SerializeField] private float FOV;
+    [SerializeField] private LayerMask targetMask;
     private Vector3 enemyDir;
-    private Color alpha;
-    private Color Nalpha;
-    private float dis;
+    private Color alpha, Nalpha;
+    private float dis, maxAngle = 50f;
+    private bool isVisible;
     private void Awake()
     {
         alpha.a = 0;
         Nalpha.a = 1;
     }
+    private void Update()
+    {
+        CheckVisibility();
+    }
     void FixedUpdate()
     {
-        dis = Vector2.Distance(enemy.transform.position, transform.position);
-        enemyDir = enemy.transform.position - transform.position;
+        Detect();
+        dis = Vector2.Distance(enemy.transform.position, transform.position); //적과 자신의 거리 구해서 dis 에 넣음
+        enemyDir = enemy.transform.position - transform.position; 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, enemyDir, dis, LayerMask.GetMask("wall"));//실제로 작동되는것
         Debug.DrawRay(transform.position, enemyDir, Color.red);//보이는것만
         if (hit.collider || (dis >= FOV))
@@ -31,6 +37,37 @@ public class EnemyDetecter : MonoBehaviour
         {
             enemy.GetComponent<SpriteRenderer>().color = Nalpha;
             enemyScript.IsAlpha = false;
+        }
+    }
+    void CheckVisibility()
+    {
+        Vector3 directionToTarget = enemy.transform.position - transform.position;
+        directionToTarget.Normalize();
+
+        Vector3 rotatedDirection = Quaternion.Euler(0, 0, 90) * transform.right;
+
+        float angle = Vector3.Angle(rotatedDirection, directionToTarget);
+
+        if (angle <= maxAngle)
+        {
+            isVisible = true;
+        }
+        else
+        {
+            isVisible = false;
+        }
+    }
+    private void Detect()
+    {
+        Collider2D[] collision = Physics2D.OverlapCircleAll(transform.position, FOV, targetMask);
+        if (collision[0])
+        {
+            print("Something detected");
+            if (collision[0].CompareTag("Enemy"))
+            {
+                print("Something is enemy");
+                enemy = collision[0].gameObject;
+            }
         }
     }
 }
